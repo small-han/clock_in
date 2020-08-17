79bb885692305ddaac3ab68da198477c2a90fddb
zhanghan <171180646@smail.nju.edu.cn>
2020-08-17 22:27:24 +0800

    update Fill_In

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 import time
 import datetime
-# 填写腾讯文档
+
 class CLOCK_IN(object):
     def __init__(self):
         self.driver = webdriver.Chrome("chromedriver.exe")
-        pass
+        self.row = 1
+        self.col = 1
 
-    def Log_In(self,url):
+    def Log_In(self, url):
         self.driver.get(url)
         element = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.ID, "header-login-btn"))
@@ -25,72 +26,73 @@ class CLOCK_IN(object):
         )
         element.click()
 
-    def Fill_In(self,row,col,data):
-        self.driver.switch_to.parent_frame() 
+    def Fill_In(self, row, col, data):
         elmet = self.driver.find_element_by_id('alloy-simple-text-editor')
+        if self.row-row > 0:
+            for i in range(self.row-row):
+                time.sleep(0.1)
+                elmet.send_keys(Keys.ARROW_UP)
+        elif self.row-row < 0:
+            for i in range(row-self.row):
+                time.sleep(0.1)
+                elmet.send_keys(Keys.ARROW_DOWN)
+        if self.col-col > 0:
+            for i in range(self.col-col):
+                time.sleep(0.1)
+                elmet.send_keys(Keys.ARROW_LEFT)
+        elif self.col-col < 0:
+            for i in range(col-self.col):
+                time.sleep(0.1)
+                elmet.send_keys(Keys.ARROW_RIGHT)
         elmet.click()
-        elmet.send_keys(Keys.ESCAPE)
-        for i in range(row-1):
-            elmet.send_keys(Keys.ARROW_DOWN)
-        for i in range(col-1):
-            elmet.send_keys(Keys.ARROW_RIGHT)
-        elmet.click()
-        elmet.send_keys(data)   # 输入xxx,即你想输入的字符
+        for i in range(5):
+            elmet.send_keys(Keys.BACKSPACE)
+        elmet.send_keys(data)
         elmet.send_keys(Keys.ENTER)
         elmet.send_keys(Keys.ARROW_UP)
-        
-        elmet.send_keys(Keys.ARROW_RIGHT)
-        elmet.click()
-        elmet.send_keys("36.7")   # 输入xxx,即你想输入的字符
-        elmet.send_keys(Keys.ENTER)
-        elmet.send_keys(Keys.ARROW_UP)
-
-
-        elmet.send_keys(Keys.ARROW_RIGHT)
-        elmet.click()
-        elmet.send_keys("1")   # 输入xxx,即你想输入的字符
-        elmet.send_keys(Keys.ENTER)
-        elmet.send_keys(Keys.ARROW_UP)
-
-        elmet.send_keys(Keys.ARROW_RIGHT)
-        elmet.click()
-        elmet.send_keys("36.7")   # 输入xxx,即你想输入的字符
-        elmet.send_keys(Keys.ENTER)
-        elmet.send_keys(Keys.ARROW_UP)
-
-        elmet.send_keys(Keys.ARROW_RIGHT)
-        elmet.click()
-        elmet.send_keys("1")   # 输入xxx,即你想输入的字符
-        elmet.send_keys(Keys.ENTER)
-        elmet.send_keys(Keys.ARROW_UP)
-        """
-        for i in range(row-1):
-            elmet.send_keys(Keys.ARROW_UP)
-        for i in range(col-1):
-            elmet.send_keys(Keys.ARROW_LEFT)
-        """
+        self.row=row
+        self.col=col
 
     def Change_Page(self):
         #element= self.driver.find_element_by_partial_link_text("sheet-tab-name")
-        self.driver.switch_to.parent_frame() 
-        #self.driver.implicitly_wait(10)
+        self.driver.switch_to.parent_frame()
+        # self.driver.implicitly_wait(10)
         time.sleep(5)
         element = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//span[text()='8.14']"))
+            EC.element_to_be_clickable((By.XPATH, "//span[text()='8.15']"))
         )
         element.click()
 
-if __name__ == "__main__" :
-    my_clock=CLOCK_IN()
+    def Fill_Ones_Message(self,row):
+        self.Fill_In(row,6,"36.7")
+        self.Fill_In(row,7,"36.7")
+        self.Fill_In(row,8,"1")
+        self.Fill_In(row,9,"36.7")
+        self.Fill_In(row,10,"1")
+    
+    def test(self):
+        elmet = self.driver.find_element_by_id('alloy-simple-text-editor')
+        """
+        for i in range(117):
+            elmet.send_keys(Keys.ARROW_DOWN)
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[text()='117']"))
+        )
+        element.click()
+        """
+        elmet.send_keys(Keys.CONTROL+"f")
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "search-panel-input"))
+        )
+
+
+
+if __name__ == "__main__":
+    my_clock = CLOCK_IN()
     my_clock.Log_In("https://docs.qq.com/sheet/DRUt0S0VnSEZKU3hV?tdsourcetag=s_pcqq_send_grpfile&ADUIN=1392104628&ADSESSION=1597489369&ADTAG=CLIENT.QQ.5749_.0&ADPUBNO=27027&tab=jiwl0l")
     my_clock.Change_Page()
-    my_clock.Fill_In(171,6,"36.7")
-    """
-    my_clock.Fill_In(171,7,"36.7")
-    my_clock.Fill_In(171,8,"1")
-    my_clock.Fill_In(171,9,"36.7")
-    my_clock.Fill_In(171,10,"1")
-    """
-    #my_clock.Fill_In(3,6,"hello")
-    print("hello")
-     
+    my_clock.test()
+"""
+    my_clock.Fill_Ones_Message(171)
+    my_clock.Fill_Ones_Message(190)
+"""
