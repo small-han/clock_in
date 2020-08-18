00022cb0d34b433c4b354c9f8a86528f9906bda5
zhanghan <171180646@smail.nju.edu.cn>
2020-08-18 23:20:17 +0800

    finish Fill_By_Name

--- main.py
+++ main.py
@@ -26,6 +26,92 @@ class CLOCK_IN(object):
         )
         element.click()
 
+    def Change_Page(self):
+        self.driver.switch_to.parent_frame()
+        time.sleep(5)
+        while True:
+            try:
+                element = WebDriverWait(self.driver, 3).until(
+                    EC.element_to_be_clickable((By.XPATH, "//span[text()='8.17']"))
+                )
+                break
+            except :
+                element = WebDriverWait(self.driver,5).until(
+                    EC.element_to_be_clickable((By.CLASS_NAME, "sheetscrollright-icon"))
+                )
+                element.click()
+        element.click()
+    
+    def Fill_By_Name(self,name):
+        elmet = self.driver.find_element_by_id('alloy-simple-text-editor')
+        elmet.send_keys(Keys.CONTROL+"f")
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "search-panel-input"))
+        )
+        element.click()
+        element.send_keys(name)
+        
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "dui-modal-close"))
+        )
+        element.click()
+
+        for i in range(5):
+            time.sleep(0.2)
+            elmet.send_keys(Keys.ARROW_RIGHT)
+
+        elmet.click()
+        for i in range(5):
+            elmet.send_keys(Keys.BACKSPACE)
+        elmet.send_keys("36.8")
+        elmet.send_keys(Keys.ENTER)
+        elmet.send_keys(Keys.ARROW_UP)
+        elmet.send_keys(Keys.ARROW_RIGHT)
+
+        elmet.click()
+        for i in range(5):
+            elmet.send_keys(Keys.BACKSPACE)
+        elmet.send_keys("36.8")
+        elmet.send_keys(Keys.ENTER)
+        elmet.send_keys(Keys.ARROW_UP)
+        elmet.send_keys(Keys.ARROW_RIGHT)
+        
+        elmet.click()
+        for i in range(5):
+            elmet.send_keys(Keys.BACKSPACE)
+        elmet.send_keys("1")
+        elmet.send_keys(Keys.ENTER)
+        elmet.send_keys(Keys.ARROW_UP)
+        elmet.send_keys(Keys.ARROW_RIGHT)
+
+        elmet.click()
+        for i in range(5):
+            elmet.send_keys(Keys.BACKSPACE)
+        elmet.send_keys("36.8")
+        elmet.send_keys(Keys.ENTER)
+        elmet.send_keys(Keys.ARROW_UP)
+        elmet.send_keys(Keys.ARROW_RIGHT)
+
+        elmet.click()
+        for i in range(5):
+            elmet.send_keys(Keys.BACKSPACE)
+        elmet.send_keys("1")
+        elmet.send_keys(Keys.ENTER)
+        elmet.send_keys(Keys.ARROW_UP)
+        elmet.send_keys(Keys.ARROW_RIGHT)
+
+if __name__ == "__main__":
+    print(datetime.datetime.now().month)
+    print(datetime.datetime.now().day)
+    my_clock = CLOCK_IN()
+    my_clock.Log_In("https://docs.qq.com/sheet/DRUt0S0VnSEZKU3hV?tdsourcetag=s_pcqq_send_grpfile&ADUIN=1392104628&ADSESSION=1597489369&ADTAG=CLIENT.QQ.5749_.0&ADPUBNO=27027&tab=jiwl0l")
+    my_clock.Change_Page()
+    my_clock.Fill_By_Name("张涵")
+    my_clock.Fill_By_Name("王春磊")
+    my_clock.Fill_By_Name("朱仁贵")
+    my_clock.Fill_By_Name("蒋赟涛")
+
+"""
     def Fill_In(self, row, col, data):
         elmet = self.driver.find_element_by_id('alloy-simple-text-editor')
         if self.row-row > 0:
@@ -52,47 +138,12 @@ class CLOCK_IN(object):
         elmet.send_keys(Keys.ARROW_UP)
         self.row=row
         self.col=col
-
-    def Change_Page(self):
-        #element= self.driver.find_element_by_partial_link_text("sheet-tab-name")
-        self.driver.switch_to.parent_frame()
-        # self.driver.implicitly_wait(10)
-        time.sleep(5)
-        element = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//span[text()='8.15']"))
-        )
-        element.click()
-
+"""
+"""
     def Fill_Ones_Message(self,row):
         self.Fill_In(row,6,"36.7")
         self.Fill_In(row,7,"36.7")
         self.Fill_In(row,8,"1")
         self.Fill_In(row,9,"36.7")
         self.Fill_In(row,10,"1")
-    
-    def test(self):
-        elmet = self.driver.find_element_by_id('alloy-simple-text-editor')
-        """
-        for i in range(117):
-            elmet.send_keys(Keys.ARROW_DOWN)
-        element = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//span[text()='117']"))
-        )
-        element.click()
-        """
-        elmet.send_keys(Keys.CONTROL+"f")
-        element = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.ID, "search-panel-input"))
-        )
-
-
-
-if __name__ == "__main__":
-    my_clock = CLOCK_IN()
-    my_clock.Log_In("https://docs.qq.com/sheet/DRUt0S0VnSEZKU3hV?tdsourcetag=s_pcqq_send_grpfile&ADUIN=1392104628&ADSESSION=1597489369&ADTAG=CLIENT.QQ.5749_.0&ADPUBNO=27027&tab=jiwl0l")
-    my_clock.Change_Page()
-    my_clock.test()
-"""
-    my_clock.Fill_Ones_Message(171)
-    my_clock.Fill_Ones_Message(190)
 """
