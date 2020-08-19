93a78d28499ee4305a1eb0d657b030f0af8a817f
zhanghan <171180646@smail.nju.edu.cn>
2020-08-19 10:01:13 +0800

    delete zip

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
@@ -13,7 +14,7 @@ import random
 class CLOCK_IN(object):
     def __init__(self):
         cp = configparser.SafeConfigParser()
-        with codecs.open('config.txt', 'r', encoding='utf-8') as f:
+        with codecs.open('config.txt', 'r', encoding='utf-8') as f:#读取config.txt数据
             cp.readfp(f)
         self.url=cp.get("data","url")
         self.qq=cp.get("data","qq_num")
@@ -26,31 +27,33 @@ class CLOCK_IN(object):
             EC.presence_of_element_located((By.ID, "header-login-btn"))
         )
         element.click()
+        #点击 "立即登录"
         self.driver.implicitly_wait(2)
         self.driver.switch_to.frame("login_frame")
         element = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.ID, "img_out_"+self.qq))
         )
         element.click()
+        #点击快捷登录的头像
 
     def Change_Page(self):
         self.driver.switch_to.parent_frame()
         time.sleep(3)
-        date=str(datetime.datetime.now().month)+"."+str(datetime.datetime.now().day)
+        date=str(datetime.datetime.now().month)+"."+str(datetime.datetime.now().day)#生成日期
         while True:
-            try:
-                element = WebDriverWait(self.driver, 2).until(
+            try:#查看在当前页面是否能找到当前日期的表格
+                element = WebDriverWait(self.driver, 2).until(#如果找到则点击
                     EC.element_to_be_clickable((By.XPATH, "//span[text()='"+date+"']"))
                 )
                 break
             except :
-                element = WebDriverWait(self.driver,5).until(
+                element = WebDriverWait(self.driver,5).until(#如果没找到则点击右滚动条
                     EC.element_to_be_clickable((By.CLASS_NAME, "sheetscrollright-icon"))
                 )
                 element.click()
         element.click()
     
-    def Random_Number(self):
+    def Random_Number(self):#生成体温随机数
         return str(random.randint(363,369)/10)
 
     def Fill_By_Name(self,name):
@@ -60,12 +63,12 @@ class CLOCK_IN(object):
             EC.element_to_be_clickable((By.ID, "search-panel-input"))
         )
         element.click()
-        element.send_keys(name)
+        element.send_keys(name)#查找名字
         
         element = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.CLASS_NAME, "dui-modal-close"))
         )
-        element.click()
+        element.click()#退出查找
 
         for i in range(5):
             time.sleep(0.4)
