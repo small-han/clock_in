c89902052ef2d659cc0e3055d8c0510f48ff1966
zhanghan <171180646@smail.nju.edu.cn>
2020-08-19 08:06:12 +0800

    code can run normally

---
+++ README.md
@@ -0,0 +1,2 @@
++ chromeDriver下载地址
+https://sites.google.com/a/chromium.org/chromedriver/downloads
--- main.py
+++ main.py
@@ -6,15 +6,18 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 import time
 import datetime
+import random
 
 class CLOCK_IN(object):
-    def __init__(self):
+    def __init__(self,qq_num,url):
         self.driver = webdriver.Chrome("chromedriver.exe")
         self.row = 1
         self.col = 1
+        self.qq=qq_num
+        self.url=url
 
-    def Log_In(self, url):
-        self.driver.get(url)
+    def Log_In(self):
+        self.driver.get(self.url)
         element = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.ID, "header-login-btn"))
         )
@@ -22,17 +25,18 @@ class CLOCK_IN(object):
         self.driver.implicitly_wait(2)
         self.driver.switch_to.frame("login_frame")
         element = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.ID, "img_out_1392104628"))
+            EC.presence_of_element_located((By.ID, "img_out_"+self.qq))
         )
         element.click()
 
     def Change_Page(self):
         self.driver.switch_to.parent_frame()
-        time.sleep(5)
+        time.sleep(3)
+        date=str(datetime.datetime.now().month)+"."+str(datetime.datetime.now().day)
         while True:
             try:
-                element = WebDriverWait(self.driver, 3).until(
-                    EC.element_to_be_clickable((By.XPATH, "//span[text()='8.17']"))
+                element = WebDriverWait(self.driver, 2).until(
+                    EC.element_to_be_clickable((By.XPATH, "//span[text()='"+date+"']"))
                 )
                 break
             except :
@@ -42,6 +46,9 @@ class CLOCK_IN(object):
                 element.click()
         element.click()
     
+    def Random_Number(self):
+        return str(random.randint(365,370)/10)
+
     def Fill_By_Name(self,name):
         elmet = self.driver.find_element_by_id('alloy-simple-text-editor')
         elmet.send_keys(Keys.CONTROL+"f")
@@ -57,13 +64,13 @@ class CLOCK_IN(object):
         element.click()
 
         for i in range(5):
-            time.sleep(0.2)
+            time.sleep(0.4)
             elmet.send_keys(Keys.ARROW_RIGHT)
 
         elmet.click()
         for i in range(5):
             elmet.send_keys(Keys.BACKSPACE)
-        elmet.send_keys("36.8")
+        elmet.send_keys(self.Random_Number())
         elmet.send_keys(Keys.ENTER)
         elmet.send_keys(Keys.ARROW_UP)
         elmet.send_keys(Keys.ARROW_RIGHT)
@@ -71,7 +78,7 @@ class CLOCK_IN(object):
         elmet.click()
         for i in range(5):
             elmet.send_keys(Keys.BACKSPACE)
-        elmet.send_keys("36.8")
+        elmet.send_keys(self.Random_Number())
         elmet.send_keys(Keys.ENTER)
         elmet.send_keys(Keys.ARROW_UP)
         elmet.send_keys(Keys.ARROW_RIGHT)
@@ -87,7 +94,7 @@ class CLOCK_IN(object):
         elmet.click()
         for i in range(5):
             elmet.send_keys(Keys.BACKSPACE)
-        elmet.send_keys("36.8")
+        elmet.send_keys(self.Random_Number())
         elmet.send_keys(Keys.ENTER)
         elmet.send_keys(Keys.ARROW_UP)
         elmet.send_keys(Keys.ARROW_RIGHT)
@@ -101,49 +108,10 @@ class CLOCK_IN(object):
         elmet.send_keys(Keys.ARROW_RIGHT)
 
 if __name__ == "__main__":
-    print(datetime.datetime.now().month)
-    print(datetime.datetime.now().day)
-    my_clock = CLOCK_IN()
-    my_clock.Log_In("https://docs.qq.com/sheet/DRUt0S0VnSEZKU3hV?tdsourcetag=s_pcqq_send_grpfile&ADUIN=1392104628&ADSESSION=1597489369&ADTAG=CLIENT.QQ.5749_.0&ADPUBNO=27027&tab=jiwl0l")
+    my_clock = CLOCK_IN("1392104628","https://docs.qq.com/sheet/DRUt0S0VnSEZKU3hV?tdsourcetag=s_pcqq_send_grpfile&ADUIN=1392104628&ADSESSION=1597489369&ADTAG=CLIENT.QQ.5749_.0&ADPUBNO=27027&tab=jiwl0l")
+    my_clock.Log_In()
     my_clock.Change_Page()
     my_clock.Fill_By_Name("张涵")
     my_clock.Fill_By_Name("王春磊")
     my_clock.Fill_By_Name("朱仁贵")
     my_clock.Fill_By_Name("蒋赟涛")
-
-"""
-    def Fill_In(self, row, col, data):
-        elmet = self.driver.find_element_by_id('alloy-simple-text-editor')
-        if self.row-row > 0:
-            for i in range(self.row-row):
-                time.sleep(0.1)
-                elmet.send_keys(Keys.ARROW_UP)
-        elif self.row-row < 0:
-            for i in range(row-self.row):
-                time.sleep(0.1)
-                elmet.send_keys(Keys.ARROW_DOWN)
-        if self.col-col > 0:
-            for i in range(self.col-col):
-                time.sleep(0.1)
-                elmet.send_keys(Keys.ARROW_LEFT)
-        elif self.col-col < 0:
-            for i in range(col-self.col):
-                time.sleep(0.1)
-                elmet.send_keys(Keys.ARROW_RIGHT)
-        elmet.click()
-        for i in range(5):
-            elmet.send_keys(Keys.BACKSPACE)
-        elmet.send_keys(data)
-        elmet.send_keys(Keys.ENTER)
-        elmet.send_keys(Keys.ARROW_UP)
-        self.row=row
-        self.col=col
-"""
-"""
-    def Fill_Ones_Message(self,row):
-        self.Fill_In(row,6,"36.7")
-        self.Fill_In(row,7,"36.7")
-        self.Fill_In(row,8,"1")
-        self.Fill_In(row,9,"36.7")
-        self.Fill_In(row,10,"1")
-"""
